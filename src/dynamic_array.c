cc2847d3e31c48fe97f7752419977605d5fb50b9
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-06-18 22:18:47 +0700

    add dynamic array source file and test file

---
+++ src/dynamic_array.c
@@ -0,0 +1,21 @@
+#include <dynamic_array.h>
+
+void *_init_dyn_array(size_t stride) {
+
+}
+
+void push_dyn_array(void *array, void *element) {
+
+}
+
+void pop_dyn_array(void *array, void *target) {
+
+}
+
+void free_dyn_array(void *array) {
+
+}
+
+size_t get_length_dyn_array(void *array) {
+    return 0;
+}
---
+++ tests/tests_dynamic_array.c
@@ -0,0 +1,5 @@
+#include <macroassert.h>
+
+void tests_dynamic_array() {
+    assert_eq(2+2, 4);
+}
