709c3165b545153710c921ee7dadff8bf31a3c06
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-06-26 12:18:57 +0700

    add physical quantities related function defenitions

---
+++ src/unit.c
@@ -0,0 +1,21 @@
+#include <unit.h>
+
+void add_comeback_to_middle_accel(Unit *unit) {
+
+}
+
+void add_friction_accel(Unit *unit) {
+
+}
+
+void add_repulsion_accel(Unit *unit, Unit *neighbor) {
+
+}
+
+void add_run_away_accel(Unit *unit, Unit *catcher) {
+
+}
+
+void set_catch_velocity(Unit *unit, Unit *catchie) {
+    
+}
--- src/unit.h
+++ src/unit.h
@@ -2,12 +2,17 @@
 #define UNIT_H
 
 #include <geometry.h>
-#include <stdbool.h>
 
 typedef struct {
     Vector position;
     Vector velocity;
     Vector acceleration;
 } Unit;
+
+void add_comeback_to_middle_accel(Unit *unit);
+void add_friction_accel(Unit *unit);
+void add_repulsion_accel(Unit *unit, Unit *neighbor);
+void add_run_away_accel(Unit *unit, Unit *catcher);
+void set_catch_velocity(Unit *unit, Unit *catchie);
 
 #endif // UNIT_H
