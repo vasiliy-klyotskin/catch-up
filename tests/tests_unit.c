cf0348d3d1486e96ed3e8ba7e86eb60ca6bbca51
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-06-26 22:40:45 +0700

    fix tests for return to middle function

--- tests/tests_unit.c
+++ tests/tests_unit.c
@@ -24,29 +24,19 @@ void test_return_to_middle_accel_when_position_is_zero() {
     assert_fp_eq(u.acceleration.y, 0);
 }
 
-void test_return_to_middle_accel() {
-    Unit u = unit_init(-2.1, 5.2);
-
-    add_return_to_middle_accel(&u, 3);
-
-    assert_fp_eq(u.acceleration.x, -6.3);
-    assert_fp_eq(u.acceleration.y, 15.6);
-}
-
-void test_return_to_middle_accel_when_accel_is_not_zero() {
-    Unit u = unit_init(-2.1, 5.2);
+void test_return_to_middle_accel_adds_when_accel_is_not_zero() {
+    Unit u = unit_init(-2, 3);
     u.acceleration.x = 5;
-    u.acceleration.y = -2;
+    u.acceleration.y = 5;
 
     add_return_to_middle_accel(&u, 3);
 
-    assert_fp_eq(u.acceleration.x, -1.3);
-    assert_fp_eq(u.acceleration.y, 13.6);
+    assert_fp_eq(u.acceleration.x, 11);
+    assert_fp_eq(u.acceleration.y, -4);
 }
 
 void tests_unit() {
     test_init();
     test_return_to_middle_accel_when_position_is_zero();
-    test_return_to_middle_accel();
-    test_return_to_middle_accel_when_accel_is_not_zero();
+    test_return_to_middle_accel_adds_when_accel_is_not_zero();
 }
