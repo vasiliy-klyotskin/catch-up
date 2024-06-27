100d8b6ec6b175d055cce50662039e4868e9db81
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-06-27 12:24:03 +0700

    add comments in tests to clerify logic

--- tests/tests_unit.c
+++ tests/tests_unit.c
@@ -18,43 +18,45 @@ void test_init(void) {
 
 void test_return_to_middle_when_position_is_zero(void) {
     Unit u = unit_init(0, 0);
-    u.acceleration = vector_init(5, 5);
 
     add_return_to_middle_accel(&u, 2);
 
-    assert_fp_eq(u.acceleration.x, 5);
-    assert_fp_eq(u.acceleration.y, 5);
+    assert_fp_eq(u.acceleration.x, 0);
+    assert_fp_eq(u.acceleration.y, 0);
 }
 
 void test_return_to_middle_when_position_is_not_zero(void) {
     Unit u = unit_init(-2, 3);
-    u.acceleration = vector_init(5, 5);
+    u.acceleration = vector_init(5, 7);
 
     add_return_to_middle_accel(&u, 2);
 
+    // 5 + -2 * -2 = 9
     assert_fp_eq(u.acceleration.x, 9);
-    assert_fp_eq(u.acceleration.y, -1);
+    // 7 + -2 * 3 = 1
+    assert_fp_eq(u.acceleration.y, 1);
 }
 
 void test_friction_when_velocity_is_zero(void) {
     Unit u = unit_init(0, 0);
-    u.acceleration = vector_init(5, 5);
 
     add_friction_accel(&u, 2);
 
-    assert_fp_eq(u.acceleration.x, 5);
-    assert_fp_eq(u.acceleration.y, 5);
+    assert_fp_eq(u.acceleration.x, 0);
+    assert_fp_eq(u.acceleration.y, 0);
 }
 
 void test_friction_when_velocity_is_not_zero(void) {
     Unit u = unit_init(0, 0);
-    u.acceleration = vector_init(5, 5);
+    u.acceleration = vector_init(5, 7);
     u.velocity = vector_init(-10, 20);
 
     add_friction_accel(&u, 2);
  
+    // 5 + -(-10) * 2 = 25
     assert_fp_eq(u.acceleration.x, 25);
-    assert_fp_eq(u.acceleration.y, -35);
+    // 7 + -20 * 2
+    assert_fp_eq(u.acceleration.y, -33);
 }
 
 void test_repulsion_if_distance_to_neighbor_is_zero(void) {
