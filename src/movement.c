b6572864b8cb6189427b5480b7b3c124003833c0
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-06-30 21:56:12 +0700

    refactoring of set_catch_velocity

--- src/movement.c
+++ src/movement.c
@@ -1,5 +1,6 @@
 #include <movement.h>
 #include <math.h>
+#include <stdbool.h>
 
 void do_euler_integration_step(Unit *const unit, const double delta) {
     unit->velocity.x += unit->acceleration.x * delta;
@@ -38,6 +39,37 @@ double min(double a, double b) {
     return (a < b) ? a : b;
 }
 
+void set_catch_initial_velocity(
+    Unit *const unit,
+    Vector disposition,
+    const double max_velocity,
+    const double velocity_increment_coef
+) {
+    Vector norm_disposition = vector_normalized(&disposition).direction;
+    const double multiplier = min(velocity_increment_coef, max_velocity);
+    unit->velocity.x = norm_disposition.x * multiplier;
+    unit->velocity.y = norm_disposition.y * multiplier;
+}
+
+void fit_current_velocity(
+    Unit *const unit,
+    Vector disposition,
+    const double max_velocity,
+    const double velocity_increment_coef,
+    const double angle_fitting_coef
+) {
+    Vector norm_velocity = vector_normalized(&unit->velocity).direction;
+    double potential_vx = unit->velocity.x + velocity_increment_coef * norm_velocity.x;
+    double max_vx = max_velocity * norm_velocity.x;
+    double potential_vy = unit->velocity.y + velocity_increment_coef * norm_velocity.y;
+    double max_vy = max_velocity * norm_velocity.y;
+    unit->velocity.x = min(max_vx, potential_vx);
+    unit->velocity.y = min(max_vy, potential_vy);
+    double radians = vector_radian(&unit->velocity, &disposition);
+    double fit_angle = radians * angle_fitting_coef;
+    unit->velocity = vector_rotated(&unit->velocity, fit_angle);
+}
+
 void set_catch_velocity(
     Unit *const unit, 
     const Unit *const catchie,
@@ -45,22 +77,11 @@ void set_catch_velocity(
     const double velocity_increment_coef,
     const double angle_fitting_coef
 ) {
-    Vector disposition = vector_difference(unit, catchie);
-    if (unit->velocity.x == 0 && unit->velocity.y == 0) {
-        NormalizedVector norm_disposition = vector_normalized(&disposition);
-        const double multiplier = min(velocity_increment_coef, max_velocity);
-        unit->velocity.x = norm_disposition.direction.x * multiplier;
-        unit->velocity.y = norm_disposition.direction.y * multiplier;
+    Vector disposition = vector_difference(&unit->position, &catchie->position);
+    bool is_velocity_zero = unit->velocity.x == 0 && unit->velocity.y == 0;
+    if (is_velocity_zero) {
+        set_catch_initial_velocity(unit, disposition, max_velocity, velocity_increment_coef);
     } else {
-        NormalizedVector norm_velocity = vector_normalized(&unit->velocity);
-        double potential_vx = unit->velocity.x + velocity_increment_coef * norm_velocity.direction.x;
-        double max_vx = max_velocity * norm_velocity.direction.x;
-        double potential_vy = unit->velocity.y + velocity_increment_coef * norm_velocity.direction.y;
-        double max_vy = max_velocity * norm_velocity.direction.y;
-        unit->velocity.x = min(max_vx, potential_vx);
-        unit->velocity.y = min(max_vy, potential_vy);
-        double radians = vector_radian(&unit->velocity, &disposition);
-        double fit_angle = radians * angle_fitting_coef;
-        unit->velocity = vector_rotated(&unit->velocity, fit_angle);
+        fit_current_velocity(unit, disposition, max_velocity, velocity_increment_coef, angle_fitting_coef);
     }
 }
