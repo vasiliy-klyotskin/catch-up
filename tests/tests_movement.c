ee52ca9f489ba2c56641bbbfcbb8a8ea57babe17
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-06-30 22:12:08 +0700

    remove comments

--- tests/tests_movement.c
+++ tests/tests_movement.c
@@ -8,13 +8,9 @@ void test_euler_integration_step(void) {
 
     do_euler_integration_step(&u, 0.1);
 
-    // -2 + 4 * 0.1 = -1.6
     assert_fp_eq(u.velocity.x, -1.6);
-    // 3 + -1 * 0.1 = 2.9
     assert_fp_eq(u.velocity.y, 2.9);
-    // 5 + -1.6 * 0.1 = 4.84
     assert_fp_eq(u.position.x, 4.84);
-    // 6 + 2.9 * 0.1 = 6.29
     assert_fp_eq(u.position.y, 6.29);
 }
 
@@ -33,9 +29,7 @@ void test_return_to_middle_when_position_is_not_zero(void) {
 
     add_return_to_middle_accel(&u, 2);
 
-    // 5 + -2 * -2 = 9
     assert_fp_eq(u.acceleration.x, 9);
-    // 7 + -2 * 3 = 1
     assert_fp_eq(u.acceleration.y, 1);
 }
 
@@ -55,9 +49,7 @@ void test_friction_when_velocity_is_not_zero(void) {
 
     add_friction_accel(&u, 2);
  
-    // 5 + -(-10) * 2 = 25
     assert_fp_eq(u.acceleration.x, 25);
-    // 7 + -20 * 2
     assert_fp_eq(u.acceleration.y, -33);
 }
 
@@ -78,9 +70,7 @@ void test_repulsion(void) {
 
     add_repulsion_accel(&u, &neighbor, 2);
 
-    // (-2 * (2 - -1) / 5 ^ 1.5) + (-3) = -3.5366563146 (where 5 is distance to neighbor)
     assert_fp_eq(u.acceleration.x, -3.5366563146);
-    // (-2 * (3 - -1) / 5 ^ 1.5) + (-3) = -3.7155417528 (where 5 is distance to neighbor)
     assert_fp_eq(u.acceleration.y, -3.7155417528);
 }
 
@@ -101,9 +91,7 @@ void test_run_away(void) {
 
     add_run_away_accel(&u, &catcher, 2);
 
-    // (-2 * (2 - -1) / 5 ^ 2) + (-3) = -3.24 (where 5 is distance to catcher)
     assert_fp_eq(u.acceleration.x, -3.24);
-    // (-2 * (3 - -1) / 5 ^ 2) + (-3) = -3.32 (where 5 is distance to catcher)
     assert_fp_eq(u.acceleration.y, -3.32);
 }
 
