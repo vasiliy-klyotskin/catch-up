3312aef7d8609124b0173d2a5ca9799d7035e1e3
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-07-02 12:53:03 +0700

    add controller files

---
+++ src/controller.h
@@ -0,0 +1,34 @@
+#if !defined(CONTROLLER_H)
+#define CONTROLLER_H
+
+#include <geometry.h>
+#include <simulation.h>
+
+typedef void (*StartFn)(void *self);
+typedef void (*DrawUnitFn)(void *self, Vector *position);
+typedef void (*UpdateScoreFn)(void *self, int score);
+typedef void (*MakeHitSoundFn)(void *self);
+typedef void (*MakeCatchSoundFn)(void *self);
+typedef void (*CleanFn)(void *self);
+
+typedef struct {
+    void *self;
+    StartFn start;
+    DrawUnitFn draw_unit;
+    UpdateScoreFn draw_score;
+    MakeHitSoundFn make_hit_sound;
+    MakeCatchSoundFn make_catch_sound;
+    CleanFn clean;
+} UI;
+
+typedef struct {
+    UI *__ui;
+    Simulation *__simulation;
+} Controller;
+
+Controller controller_init(UI *ui, Simulation *simulation);
+void controller_start(Controller *controller);
+void controller_add_runner(Controller *controller, Vector position);
+void controller_clean(Controller *controller);
+
+#endif // CONTROLLER_H
