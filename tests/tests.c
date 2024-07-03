de8341a86e9dcb793d3175d3b5d651a5af27dbae
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-07-04 00:55:47 +0700

    move unit tests from movement file to unit file

--- tests/tests.c
+++ tests/tests.c
@@ -4,13 +4,11 @@ void tests_dynamic_array(void);
 void tests_geometry(void);
 void tests_unit(void);
 void tests_collision(void);
-void tests_movement(void);
 
 int main(void) {
     tests_dynamic_array();
     tests_geometry();
     tests_unit();
-    tests_movement();
     tests_collision();
     printf("All tests passed!");
     return 0;
